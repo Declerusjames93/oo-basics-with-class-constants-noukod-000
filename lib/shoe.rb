11e56222ba2ea56dce5031483c906ca9c709f357
James Declerus <james.declerus@esih.edu>
2019-04-24 12:56:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,10 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  brands = []
+  BRANDS = []
   def initialize(brand)
     @brand = brand
-
-   brands << brand unless BRANDS.any? { |b| b == brand }
+   BRANDS << brand unless BRANDS.any? { |b| b == brand }
   end
 
   def cobble
